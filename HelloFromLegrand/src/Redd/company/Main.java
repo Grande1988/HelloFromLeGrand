e9412ae8a3f5ba709b9015c3113a204aa5ec36d2
dstevenvg <stevensvg59@gmail.com>
2021-05-19 22:17:17 -0500

    I've just added my name -> Steven

--- HelloFromLegrand/src/Redd/company/Main.java
+++ HelloFromLegrand/src/Redd/company/Main.java
@@ -3,6 +3,8 @@ package Redd.company;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is LeGrand!");
+        System.out.println("Hello LeGrand, my name is Steven");
     }
 }
